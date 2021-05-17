a346b1994a68caf7027123db118fac6dbd5a3cce
Ron Ilan <github@ronilan.com>
2021-05-16 23:26:09 -0700

    Upgraded webpack for docs.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: 'production',
   entry: path.join(__dirname, "src/docs"),
   output: {
     path: path.join(__dirname, "docs"),
@@ -29,11 +29,6 @@ module.exports = {
       }
     ]
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src/docs/index.html")
-    })
-  ],
   resolve: {
     extensions: [".js", ".jsx"]
   },
