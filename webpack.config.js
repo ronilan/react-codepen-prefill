ae85814afc609a11052317b07e3378d0d54e17bf
Ron Ilan <github@ronilan.com>
2022-08-09 22:52:33 -0700

    webpack to latest

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
     extensions: [".js", ".jsx"]
   },
   devServer: {
-    contentBase: path.join(__dirname, "docs"),
+    static: path.join(__dirname, "docs"),
     port: 8000
   }
 };
